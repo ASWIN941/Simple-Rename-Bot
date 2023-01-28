c941cc2b47081c4f0acf73826c0018b781dcb4f4
ASWIN941 <84381878+ASWIN941@users.noreply.github.com>
2023-01-28 15:53:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "17472409"))
+API_HASH = environ.get("API_HASH", "f88f6e75e24743472483687be021b684")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5822099090:AAGS9NIGy5tQmJmHUofmGnbDsGU6RhpC_Fo")
+ADMIN = int(environ.get("ADMIN", "5377042833"))          
+CAPTION = environ.get("CAPTION", "@Anime_LandXD")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "")
